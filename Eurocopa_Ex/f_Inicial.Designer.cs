a36bfdea61be9f107b1564274cbafec3ae554751
Jpchapel <jpchapel82@gmail.com>
2019-11-13 23:28:20 +0100

    Version 0.0

---
+++ Eurocopa_Ex/f_Inicial.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Eurocopa
+{
+    partial class f_Inicial
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(f_Inicial));
+            this.lbxEquipos = new System.Windows.Forms.ListBox();
+            this.btGrupos = new System.Windows.Forms.Button();
+            this.lbxGrupo1 = new System.Windows.Forms.ListBox();
+            this.lbxGrupo2 = new System.Windows.Forms.ListBox();
+            this.btCalendario = new System.Windows.Forms.Button();
+            this.lbxCalendarioG2 = new System.Windows.Forms.ListBox();
+            this.lbxCalendarioG1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbxEquipos
+            // 
+            this.lbxEquipos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxEquipos.FormattingEnabled = true;
+            this.lbxEquipos.ItemHeight = 16;
+            this.lbxEquipos.Location = new System.Drawing.Point(12, 180);
+            this.lbxEquipos.Name = "lbxEquipos";
+            this.lbxEquipos.Size = new System.Drawing.Size(181, 340);
+            this.lbxEquipos.TabIndex = 0;
+            // 
+            // btGrupos
+            // 
+            this.btGrupos.Enabled = false;
+            this.btGrupos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btGrupos.Location = new System.Drawing.Point(213, 74);
+            this.btGrupos.Name = "btGrupos";
+            this.btGrupos.Size = new System.Drawing.Size(120, 48);
+            this.btGrupos.TabIndex = 1;
+            this.btGrupos.Text = "sortear grupos";
+            this.btGrupos.UseVisualStyleBackColor = true;
+            this.btGrupos.Click += new System.EventHandler(this.btGrupos_Click);
+            // 
+            // lbxGrupo1
+            // 
+            this.lbxGrupo1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxGrupo1.FormattingEnabled = true;
+            this.lbxGrupo1.ItemHeight = 15;
+            this.lbxGrupo1.Location = new System.Drawing.Point(344, 74);
+            this.lbxGrupo1.Name = "lbxGrupo1";
+            this.lbxGrupo1.Size = new System.Drawing.Size(181, 169);
+            this.lbxGrupo1.TabIndex = 2;
+            // 
+            // lbxGrupo2
+            // 
+            this.lbxGrupo2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxGrupo2.FormattingEnabled = true;
+            this.lbxGrupo2.ItemHeight = 15;
+            this.lbxGrupo2.Location = new System.Drawing.Point(546, 74);
+            this.lbxGrupo2.Name = "lbxGrupo2";
+            this.lbxGrupo2.Size = new System.Drawing.Size(181, 169);
+            this.lbxGrupo2.TabIndex = 3;
+            // 
+            // btCalendario
+            // 
+            this.btCalendario.Enabled = false;
+            this.btCalendario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCalendario.Location = new System.Drawing.Point(213, 290);
+            this.btCalendario.Name = "btCalendario";
+            this.btCalendario.Size = new System.Drawing.Size(120, 48);
+            this.btCalendario.TabIndex = 4;
+            this.btCalendario.Text = "xerar calendario";
+            this.btCalendario.UseVisualStyleBackColor = true;
+            this.btCalendario.Click += new System.EventHandler(this.btCalendario_Click);
+            // 
+            // lbxCalendarioG2
+            // 
+            this.lbxCalendarioG2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxCalendarioG2.FormattingEnabled = true;
+            this.lbxCalendarioG2.ItemHeight = 15;
+            this.lbxCalendarioG2.Location = new System.Drawing.Point(546, 290);
+            this.lbxCalendarioG2.Name = "lbxCalendarioG2";
+            this.lbxCalendarioG2.Size = new System.Drawing.Size(181, 229);
+            this.lbxCalendarioG2.TabIndex = 6;
+            // 
+            // lbxCalendarioG1
+            // 
+            this.lbxCalendarioG1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxCalendarioG1.FormattingEnabled = true;
+            this.lbxCalendarioG1.ItemHeight = 15;
+            this.lbxCalendarioG1.Location = new System.Drawing.Point(344, 290);
+            this.lbxCalendarioG1.Name = "lbxCalendarioG1";
+            this.lbxCalendarioG1.Size = new System.Drawing.Size(181, 229);
+            this.lbxCalendarioG1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.SteelBlue;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 26);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "equipos participantes";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.SteelBlue;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(344, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 26);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Grupo 1";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.SteelBlue;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(546, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 26);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Grupo 2";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.SteelBlue;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(344, 264);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(182, 26);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "calendario G1";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.SteelBlue;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(546, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(182, 26);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "calendario G2";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(7, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(295, 85);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.label6.Location = new System.Drawing.Point(2, 1);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(752, 95);
+            this.label6.TabIndex = 13;
+            // 
+            // f_Inicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 532);
+            this.Controls.Add(this.btGrupos);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbxCalendarioG2);
+            this.Controls.Add(this.lbxCalendarioG1);
+            this.Controls.Add(this.btCalendario);
+            this.Controls.Add(this.lbxGrupo2);
+            this.Controls.Add(this.lbxGrupo1);
+            this.Controls.Add(this.lbxEquipos);
+            this.Controls.Add(this.label6);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "f_Inicial";
+            this.Text = "Campeonato de Europa";
+            this.Load += new System.EventHandler(this.f_Inicial_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxEquipos;
+        private System.Windows.Forms.Button btGrupos;
+        private System.Windows.Forms.ListBox lbxGrupo1;
+        private System.Windows.Forms.ListBox lbxGrupo2;
+        private System.Windows.Forms.Button btCalendario;
+        private System.Windows.Forms.ListBox lbxCalendarioG2;
+        private System.Windows.Forms.ListBox lbxCalendarioG1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Eurocopa_Ex/f_Inicial.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Eurocopa
+{
+    public partial class f_Inicial : Form
+    {
+        public f_Inicial()
+        {
+            InitializeComponent();
+        }
+
+        string[,] aEquipos = new string[20, 2] {{"Austria",""},
+                                                {"Bélgica",""},
+                                                {"Croacia",""},
+                                                {"Inglaterra",""},
+                                                {"Francia",""},
+                                                {"Alemania",""},
+                                                {"Hungría",""},
+                                                {"Islandia",""},
+                                                {"Italia",""},
+                                                {"Polonia",""},
+                                                {"Portugal",""},
+                                                {"Irlanda",""},
+                                                {"Rumania",""},
+                                                {"Rusia",""},
+                                                {"Eslovaquia",""},
+                                                {"España",""},
+                                                {"Suecia",""},
+                                                {"Suiza",""},
+                                                {"Turquía",""},
+                                                {"Ucrania",""}};
+
+        int[] aGrupo1 = new int[10];
+        int[] aGrupo2 = new int[10];
+        int[,] aCalendario = new int[90, 4];
+
+        int npartido = 0;
+        int xornada = 1;
+
+        private void f_Inicial_Load(object sender, EventArgs e)
+        {
+            Cargargrupos();
+
+            btGrupos.Enabled = true;
+        }
+
+        private void Cargargrupos()
+        {
+            for (int i = 0; i < aEquipos.GetLength(0); i++)
+            {
+                aEquipos[i, 1] = aEquipos[i, 0].Substring(0, 3).ToUpper();
+                lbxEquipos.Items.Add(aEquipos[i, 1] + "\t" + aEquipos[i, 0]);
+            }
+        }
+
+        private void btGrupos_Click(object sender, EventArgs e)
+        {
+            SortearGrupos();
+            CargarGruposSorteados();
+
+            btCalendario.Enabled = true;
+            btGrupos.Enabled = false;
+        }
+
+        private void SortearGrupos()
+        {
+            Random r = new Random();
+            int nEquipoR;
+
+            for (int i = 0; i < aEquipos.GetLength(0); i++)
+            {
+                nEquipoR = r.Next(20);
+                if (i < 10)
+                {
+                    if (Array.IndexOf(aGrupo1, nEquipoR, 0, i) >= 0)
+                    {
+                        i--;
+                    }
+                    else
+                    {
+                        aGrupo1[i] = nEquipoR;
+                    }
+
+                }
+                else
+                {
+                    if (Array.IndexOf(aGrupo1, nEquipoR, 0, aGrupo1.GetLength(0)) >= 0 || Array.IndexOf(aGrupo2, nEquipoR, 0, i - 10) >= 0)
+                    {
+                        i--;
+                    }
+                    else
+                    {
+                        aGrupo2[i - 10] = nEquipoR;
+                    }
+                }
+            }
+        }
+
+        private void CargarGruposSorteados()
+        {
+            for (int i = 0; i < aGrupo1.GetLength(0); i++)
+            {
+                lbxGrupo1.Items.Add((aEquipos[aGrupo1[i], 1]) + "\t" + (aEquipos[aGrupo1[i], 0]));
+                lbxGrupo2.Items.Add((aEquipos[aGrupo2[i], 1]) + "\t" + (aEquipos[aGrupo2[i], 0]));
+            }
+        }
+
+        private void btCalendario_Click(object sender, EventArgs e)
+        {
+            xerarXornada(aGrupo1, 1);
+            xerarXornada(aGrupo2, 2);
+
+            GenerarPartidosJornada();
+
+            CargarJornada();
+            btCalendario.Enabled = false;
+        }
+
+        private void xerarXornada(int[] aGR, int grupo)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                aCalendario[npartido, 0] = xornada;
+                aCalendario[npartido, 1] = grupo;
+                aCalendario[npartido, 2] = aGR[i];
+                aCalendario[npartido, 3] = aGR[9 - i];
+                npartido++;
+            }
+        }
+
+        private void GenerarPartidosJornada()
+        {
+            int tempGrupo1;
+            int tempGrupo2;
+
+            for (xornada = 2; xornada < 10; xornada++)
+            {
+                tempGrupo1 = aGrupo1[9];
+                tempGrupo2 = aGrupo2[9];
+
+                for (int i = 9; i > 1; i--)
+                {
+                    aGrupo1[i] = aGrupo1[i - 1];
+                    aGrupo2[i] = aGrupo2[i - 1];
+                }
+
+                aGrupo1[1] = tempGrupo1;
+                aGrupo2[1] = tempGrupo2;
+
+                xerarXornada(aGrupo1, 1);
+                xerarXornada(aGrupo2, 2);
+            }
+        }
+
+        private void CargarJornada()
+        {
+            for (int i = 0; i < aCalendario.GetLength(0); i++)
+            {
+                if (i % 10 == 0)
+                {
+                    lbxCalendarioG1.Items.Add("-------------------");
+                    lbxCalendarioG1.Items.Add("xornada " + aCalendario[i, 0]);
+                    lbxCalendarioG1.Items.Add("-------------------");
+
+                    lbxCalendarioG2.Items.Add("-------------------");
+                    lbxCalendarioG2.Items.Add("xornada " + aCalendario[i, 0]);
+                    lbxCalendarioG2.Items.Add("-------------------");
+                }
+
+                if (aCalendario[i, 1] == 1)
+                {
+                    lbxCalendarioG1.Items.Add(aEquipos[aCalendario[i, 2], 1] + "\t" + aEquipos[aCalendario[i, 3], 1]);
+                }
+
+                else
+                {
+                    lbxCalendarioG2.Items.Add(aEquipos[aCalendario[i, 2], 1] + "\t" + aEquipos[aCalendario[i, 3], 1]);
+                }
+            }
+        }
+    }
+}
